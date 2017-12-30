29751af2d00d53c08743910318768d651e65c3cb
wojtek <wojtek.klusek2@gmail.com>
2017-12-30 17:55:23 +0100

    Fields fix

--- Eurobusiness test/fields.hpp
+++ Eurobusiness test/fields.hpp
@@ -7,31 +7,31 @@ class Field
 public:
     int id;
     string name;
-    GLfloat position_x_start;
-    GLfloat position_x_end;
-    GLfloat position_y_start;
-    GLfloat position_y_end;
-    GLfloat position_z_start;
-    GLfloat position_z_end;
+    GLfloat p1_position_x;
+    GLfloat p1_position_y;
+    GLfloat p1_position_z;
+    GLfloat p2_position_x;
+    GLfloat p2_position_y;
+    GLfloat p2_position_z;
+
     float prize;
     int owner;   ///-1 none, 1 player1, 2 player2
 
     Field(int id, string name,
-           GLfloat position_x_start, GLfloat position_y_start, GLfloat position_z_start,
-           GLfloat position_x_end, GLfloat position_y_end, GLfloat position_z_end,
+           GLfloat p1_position_x, GLfloat p1_position_y, GLfloat p1_position_z,
+           GLfloat p2_position_x, GLfloat p2_position_y, GLfloat p2_position_z,
            float prize, int owner)
     {
         this->id=id;
         this->name=name;
 
-        this->position_x_start=position_x_start;
-        this->position_x_end=position_x_end;
+        this->p1_position_x=p1_position_x;
+        this->p1_position_y=p1_position_y;
+        this->p1_position_z=p1_position_z;
 
-        this->position_y_start=position_y_start;
-        this->position_y_end=position_y_end;
-
-        this->position_z_start=position_z_start;
-        this->position_z_end=position_z_end;
+        this->p2_position_x=p2_position_x;
+        this->p2_position_y=p2_position_x;
+        this->p2_position_z=p2_position_x;
 
         this->prize=prize;
         this->owner=owner;
