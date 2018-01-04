44d60ee1289ca8cbe96ddb7b8a31af726ebd78ba
wojtek <wojtek.klusek2@gmail.com>
2018-01-04 15:53:08 +0100

    Create Fields

--- Eurobusiness/fields.hpp
+++ Eurobusiness/fields.hpp
@@ -6,7 +6,6 @@ using namespace std;
 class Field
 {
 public:
-    int id;
     string name;
     GLfloat p1_position_x;
     GLfloat p1_position_y;
@@ -18,12 +17,11 @@ public:
     float prize;
     int owner;   ///-1 none, 1 player1, 2 player2
 
-    Field(int id, string name,
+    Field( string name,
            GLfloat p1_position_x, GLfloat p1_position_y, GLfloat p1_position_z,
            GLfloat p2_position_x, GLfloat p2_position_y, GLfloat p2_position_z,
            float prize, int owner)
     {
-        this->id=id;
         this->name=name;
 
         this->p1_position_x=p1_position_x;
